27e0e865c6bafd35b6d9df9d190493ced7860727
CodeDooku <116918701+CodeDooku@users.noreply.github.com>
2023-02-08 22:19:43 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -62,7 +62,6 @@ public class Main {
             } else if (binaryInput.charAt(i) == '1') {
                 enCodedString.append("0 ");
             }
-            //should be obvious if you are sober
             int count = 1;
             while (i + 1 < binaryInput.length() && binaryInput.charAt(i) == binaryInput.charAt(i + 1)) {
                 i++;
